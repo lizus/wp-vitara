0581b5faf1366125385c56676b55f1028a38b7df
lizus <lizus.com@gmail.com>
2024-02-25 12:06:14 +0800

    modified:   src/Post/QueryPost.php

--- src/Post/QueryPost.php
+++ src/Post/QueryPost.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace LizusVitara\Post;
 
 /**
@@ -6,49 +7,51 @@ namespace LizusVitara\Post;
  * WP_Query查询类，子类在继承的时候只需实现get_item即可
  * 主题包中必须有\App\QueryPost\QueryPost
  */
-abstract class QueryPost extends \LizusVitara\Model\QueryData 
+abstract class QueryPost extends \LizusVitara\Model\QueryData
 {
-    protected function default_args(){
+    protected function default_args()
+    {
         return [
-            'posts_per_page'=>1,
-            'post_type'=>'post',
-            'post_status'=>'publish',
-            'orderby'=>'date',
-            'order'=>'DESC',
+            'posts_per_page' => 1,
+            'post_type' => 'post',
+            'post_status' => 'publish',
+            'orderby' => 'date',
+            'order' => 'DESC',
         ];
     }
-    
-    protected function query(){
-        $pq=new \WP_Query($this->args);
-        $data=[];
+
+    protected function query()
+    {
+        $pq = new \WP_Query($this->args);
+        $data = [];
         //可以内存更小，但失去一些灵活性
-        /*
         foreach ($pq->posts as $item) {
-            $data[]=[
-                'pid'=>$item->ID,
-                'post_type'=>$item->post_type,
+            $data[] = [
+                'pid' => $item->ID,
+                'post_type' => $item->post_type,
             ];
         }
-        */
-        if ($pq->have_posts()){
-            while ($pq->have_posts()){
+        /*
+        if ($pq->have_posts()) {
+            while ($pq->have_posts()) {
                 $pq->the_post();
-                $data[]=$this->get_item(get_the_ID());
+                $data[] = $this->get_item(get_the_ID());
             }
         }
         wp_reset_postdata();
+        */
         return [
-            'data'=>$data,
-            'total'=>$pq->found_posts,
-            'count'=>$pq->post_count,
+            'data' => $data,
+            'total' => $pq->found_posts,
+            'count' => $pq->post_count,
         ];
     }
-    
+
     /**
-    * get_item
-    * 在WP_Query的loop中对每一个post进行数据清洗，获得需要的信息
-    * @param  int $id
-    * @return array
-    */
+     * get_item
+     * 在WP_Query的loop中对每一个post进行数据清洗，获得需要的信息
+     * @param  int $id
+     * @return array
+     */
     abstract protected function get_item($id);
 }
